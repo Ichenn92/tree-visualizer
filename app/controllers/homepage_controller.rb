87aff632b782fff276034f52924ba9dcb400ffb7
Tristan Hörler <tristanhorler@imac.home>
2020-09-22 23:14:48 +0200

    Test binary tree display and functions on homepage with the tree_handler gem

--- app/controllers/homepage_controller.rb
+++ app/controllers/homepage_controller.rb
@@ -1,5 +1,9 @@
 class HomepageController < ApplicationController
   def index
-    @welcome_message = "Welcome to you!"
+    @welcome_message = 'Welcome to you!'
+    @based_tree = [3,6,2,78,24,1]
+    tree = TreeHandler::Tree.new(@based_tree)
+    tree.rebalance!
+    @tree = tree.level_order.flatten
   end
 end
