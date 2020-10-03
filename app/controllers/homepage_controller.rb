7fcbeafc978c9e58c5bcd9a85c59430e43c9341a
Tristan Hörler <tristan.horler@gmail.com>
2020-10-03 22:54:06 +0200

    create custom TreesApi class and update TreeHandler gem to newer version

--- app/controllers/homepage_controller.rb
+++ app/controllers/homepage_controller.rb
@@ -2,7 +2,7 @@ class HomepageController < ApplicationController
   def index
     @welcome_message = 'Welcome to you!'
     @based_tree = [3,6,2,78,24,1]
-    tree = TreeHandler::Tree.new(@based_tree)
+    tree = TreesApi.new(@based_tree)
     tree.rebalance!
     @tree = tree.level_order.flatten
   end
